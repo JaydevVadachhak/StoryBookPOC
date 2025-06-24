0c6e8e27bab52da0533010c264183f01c5270307
Jaydev Vadachhak <jaydevvadachhak@gmail.com>
2025-06-25 00:13:33 +0530

    chore: enhance Storybook configuration by adding styling support and updating angular.json

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,8 @@ const config: StorybookConfig = {
   ],
   "addons": [
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
+    "@storybook/addon-onboarding",
+    "@storybook/addon-styling-webpack"
   ],
   "framework": {
     "name": "@storybook/angular",
